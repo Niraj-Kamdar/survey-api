4961872e8f0d5b10678b8a780b1a5d8fa016da4e
Sourcery AI <>
2020-10-15 13:35:41 +0000

    'Refactored by Sourcery'

--- app/utils.py
+++ app/utils.py
@@ -33,5 +33,4 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
