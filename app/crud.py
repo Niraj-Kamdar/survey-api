f059e0948acbbccdca3ebdeb0a6c9fa9cf5bc04b
Restyled.io <commits@restyled.io>
2020-10-16 12:02:03 +0000

    Restyled by autopep8

--- app/crud.py
+++ app/crud.py
@@ -113,15 +113,16 @@ def get_survey_result(db: Session, survey_id: int):
             stats[question.question].agree += response.answer
             result[response.user.username][question.question] = response.answer
         stats[question.question].percentage = (
-                                                      stats[question.question].agree /
-                                                      stats[question.question].total
-                                              ) * 100
+            stats[question.question].agree /
+            stats[question.question].total
+        ) * 100
     responses = [
         schemas.UserResponse(username=username, response=response)
         for username, response in result.items()
     ]
 
-    survey = db.query(models.Survey).filter(models.Survey.id == survey_id).first()
+    survey = db.query(models.Survey).filter(
+        models.Survey.id == survey_id).first()
     return schemas.SurveyResult(
         title=survey.title,
         description=survey.description,
